5b0d5f4968801529c5f5255de68ac19f2eba62a3
MiguelAngelToscuento <migueltoscuento52@gmail.com>
2026-03-27 02:13:23 -0600

    [MiguelAngelToscuento] revisar si se agreg[o grego el cambio en hola.java

--- hola.java
+++ hola.java
@@ -4,5 +4,6 @@ public class hola {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("esto es una modificacion para agregar cambios en git");
+        sSystem.out.println("Hola esto es otro mensaje"); 
     }
 }
